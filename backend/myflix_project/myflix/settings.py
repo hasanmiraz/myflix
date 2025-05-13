23e013d1812db10113c061c02d867d658312cc00
Hasan Miraz <hasibul.miraz@gmail.com>
2025-05-14 01:28:42 +1000

    rest framework

--- backend/myflix_project/myflix/settings.py
+++ backend/myflix_project/myflix/settings.py
@@ -42,7 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
